61f28c2d0a478a5fc859b60ee9d6543add7d9080
Harjot Singh <123859955+Harry30363@users.noreply.github.com>
2024-03-27 04:58:11 +0000

    done

---
+++ Javascript/2_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  //treat all js code as newer version
+//alert( 3 + 3 ) we are using nodejs not browser
+
+let name="hh"
+let age=18
+let isLoggedin=false
+
+// Number=> 2 to power 53
+// bigint
+// sting=>" "
+// boolean=> true / false
+// null=> stand alone value
+// undefined =>
+// symbol =>unique
+
+console.log(typeof null) // type is object
+console.log(typeof undefined); // type is undefined
